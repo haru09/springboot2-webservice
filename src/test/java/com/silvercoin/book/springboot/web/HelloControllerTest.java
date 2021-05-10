0221dbb8209f8250bea3de515edc2606dc2d9ce8
haru09 <lanterlt@naver.com>
2021-05-10 21:32:27 +0900

    HelloController 테스트코드 작성

---
+++ src/test/java/com/silvercoin/book/springboot/web/HelloControllerTest.java
@@ -0,0 +1,30 @@
+package com.silvercoin.book.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)        // JUnit에 내장된 실행자 외 다른 실행자 실행(여기선 SpringRunner)
+@WebMvcTest     // Web(Spring MVC)에 집중할 수 있는 어노테이션, @Controller, ControllerAdvice 사용
+                // Component, Repository 사용 불가
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
